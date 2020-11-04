f87d3e4d95044856c39c1fa53e84c03ee7332334
shehan82 <shehansandeepa82@gmail.com>
2020-11-04 14:16:52 +0530

    added bootstrap

---
+++ src/components/getDetails.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const GetDetails = () => {
+    return (
+       <React.Fragment>
+          <button className="btn btn-primary">hello</button>
+       </React.Fragment>
+    )
+}
+
+export default GetDetails;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GetDetails from './components/getDetails'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GetDetails />
   </React.StrictMode>,
   document.getElementById('root')
 );
